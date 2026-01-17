2026bae76ed018df1268cb51160d40c1f81778fd
fab_rohit03 <rohitchandel2024@gmail.com>
2026-01-17 11:56:39 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (75.86%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count=0;
+       
+        for(int i=s.length()-1;i>=0 ;i--){
+             char ch =s.charAt(i);
+            if(ch==' ' && count==0){
+                continue;
+                }
+                if(ch==' ' && count>0){
+                    break;
+                }
+                count++;
+        }return count;
+    }
+}
