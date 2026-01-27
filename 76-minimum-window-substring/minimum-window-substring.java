851ab9d8467a282b38bdaf26c53537eb8919d93f
fab_rohit03 <rohitchandel2024@gmail.com>
2026-01-27 21:19:23 +0530

    Time: 48 ms (18.42%) | Memory: 46.7 MB (63.20%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,44 @@
+class Solution {
+
+    private boolean sahi(int[] have, int[] needed) {
+        for (int i = 0; i < 256; i++) {
+            if (have[i] < needed[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String minWindow(String s, String t) {
+        if (s.length() == 0 || t.length() == 0) return "";
+
+        int[] needed = new int[256];
+        int[] have = new int[256];
+
+        // count t
+        for (char c : t.toCharArray()) {
+            needed[c]++;
+        }
+
+        int low = 0;
+        int start = 0;
+        int res = Integer.MAX_VALUE;
+
+        for (int high = 0; high < s.length(); high++) {
+            have[s.charAt(high)]++;
+
+            while (sahi(have, needed)) {
+                int len = high - low + 1;
+                if (len < res) {
+                    res = len;
+                    start = low;
+                }
+
+                have[s.charAt(low)]--;
+                low++;
+            }
+        }
+
+        return res == Integer.MAX_VALUE ? "" : s.substring(start, start + res);
+    }
+}
