e8b6aff2c3202c6f0db3d8c7503ca6b5f4fc4610
fab_rohit03 <rohitchandel2024@gmail.com>
2026-01-27 23:12:46 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (11.17%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.java
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -3,20 +3,23 @@ class Solution {
         int n=code.length;
         int res[]=new int [n];
         if(k==0) return res;
+        int sum=0;
+        int start=1;
+        int end=k;
+        if(k<0){
+            start=n+k;
+            end=n-1;
+        }
+        for(int i=start;i<=end;i++){
+            sum+=code[i];
+        }
         for(int i=0;i<n;i++){
-            int sum=0;
-            
-            if(k>0){
-                for(int j=1;j<=k;j++){
-                    sum+=code[(i+j)%n];
-                }}
-                else{
-                    for(int j=1;j<=-k;j++){
-                        sum+=code[(i-j+n)%n];
-                    }
-                
-            }
             res[i]=sum;
+            sum-=code[start%n];
+            sum+=code[(end+1)%n];
+            start++;
+            end++;
+            
         }
         return res;
     }
