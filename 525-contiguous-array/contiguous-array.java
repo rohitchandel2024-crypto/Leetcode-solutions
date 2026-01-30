6acdb9ca1f3bb049c4d3c039c006549f3e85c9ff
fab_rohit03 <rohitchandel2024@gmail.com>
2026-01-30 11:49:02 +0530

    Time: 24 ms (63.67%) | Memory: 65.6 MB (33.63%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -1,29 +1,23 @@
 class Solution {
     public int findMaxLength(int[] nums) {
         HashMap<Integer,Integer>freq=new HashMap<>();
-        int zero=0;
-        int one=0;
-        int res=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                zero++;
-            }
-            else{
-                one++;
-            }
-            int diff=one-zero;
-            if(diff==0){
-            res=Math.max(res,i+1);
-            
-            }
-            if(!freq.containsKey(diff)){
-                freq.put(diff,i);
-            }
-            else{
-                int ind=freq.get(diff);
-                int len=i-ind;
-               res=Math.max(len,res);
-            }
-        }return res;
+       int one =0;
+       int zero=0;
+       int res=0;
+       for(int i=0;i<nums.length;i++){
+        if(nums[i]==0) zero++;
+        else one++;
+        
+        int diff=zero-one;
+        if(diff==0) res=Math.max(res,i+1);
+        if(!freq.containsKey(diff)) freq.put(diff,i);
+        else{
+            int ind =freq.get(diff);
+            int len=i-ind;
+            res=Math.max(len,res);
+        }
+
+       }
+       return res;
     }
 }
