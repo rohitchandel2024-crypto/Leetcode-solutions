1e8f557ee055a2633e410c01df39ebd2beb98fdc
Rohit Chandel <rohitchandel2024@gmail.com>
2026-02-02 10:30:13 +0530

    Time: 12 ms (18.17%) | Memory: 44.2 MB (25.02%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,25 +1,24 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-
-        HashMap<Character,Integer> have = new HashMap<>();
-        for(int i=0;i<text.length();i++){
-            char ch=text.charAt(i);
-            have.put(ch,have.getOrDefault(ch,0)+1);
-        }
-        HashMap<Character,Integer> need = new HashMap<>();
-           need.put('b',1);
-           need.put('a',1);
-           need.put('l',2);
-           need.put('o',2);
-           need.put('n',1);
-
-        int res =Integer.MAX_VALUE;
-        
-        for(Map.Entry<Character,Integer>entry:need.entrySet()){
-            char c =entry.getKey();
-            int required = entry.getValue();
-            int available= have.getOrDefault(c,0);
-            res =Math.min(res,available/required);
-        }return res;
+    HashMap<Character,Integer> have=new HashMap<>();
+    int n=text.length();
+    for(int i=0;i<n;i++){
+        char ch =text.charAt(i);
+        have.put(ch,have.getOrDefault(ch,0)+1);
+    }
+    HashMap<Character,Integer> need=new HashMap<>();
+    need.put('b',1);
+    need.put('a',1);
+    need.put('l',2);
+    need.put('o',2);
+    need.put('n',1);
+    int res=Integer.MAX_VALUE;
+    for(Map.Entry<Character,Integer>entry:need.entrySet()){
+        char ch=entry.getKey();
+        int required=entry.getValue();
+        int available=have.getOrDefault(ch,0);
+        res=Math.min(res,available/required);
+    }
+    return res;
     }
 }
