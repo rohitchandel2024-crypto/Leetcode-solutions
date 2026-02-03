6ed382fafc74a5505d17315c8f119c7883939e48
Rohit Chandel <rohitchandel2024@gmail.com>
2026-02-03 17:28:21 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (12.35%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -1,50 +1,45 @@
 class Solution {
 
-    ListNode reverse(ListNode head, int k){
-        ListNode curr = head;
+    ListNode reverse(ListNode head,int size){
+        ListNode curr=head;
         ListNode prev=null;
-        while(k-- >0){
-            ListNode next =curr.next;
+        while(size-- >0){
+            ListNode next=curr.next;
             curr.next=prev;
             prev=curr;
             curr=next;
-        }return prev;
+        }
+        return prev;
     }
 
-
     public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode left= head;
-        ListNode right;
-        ListNode prevLeft =null;
+        ListNode left=head;
         ListNode res=null;
-        while(true){
-            right=left;
-            boolean valid =true;
-        for(int i=0;i<(k-1);i++){
-            if(right==null)
-              {
-                valid=false;
-                break; 
-                }
+        ListNode prevLeft=null;
+       
+        int size=k;
+     while(true){
+         ListNode right=left;
+        for(int i=0;i<k;i++){
+            if(right==null){
+                return res!=null ? res: head;
+            }
             right=right.next;
-
         }
-        if(!valid || right == null) break;
 
-    
-        ListNode nextLeft = right.next;
-        ListNode newHead = reverse(left,k);
+        ListNode nextLeft=right;
+        ListNode newHead= reverse(left,size);
         if(prevLeft!=null){
             prevLeft.next=newHead;
         }
         prevLeft=left;
-        if(res==null) res=newHead;
+        if(res==null){
+            res=newHead;
+        }
         left.next=nextLeft;
         left=nextLeft;
-       
-    
+        
     }
-
-    return res!=null ? res : head;
-}
+    }
+      
 }
