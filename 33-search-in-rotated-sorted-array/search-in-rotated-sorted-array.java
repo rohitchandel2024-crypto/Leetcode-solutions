a788bdf4a81efd69e209d12ef5085ec77c01dfee
Rohit Chandel <rohitchandel2024@gmail.com>
2026-02-04 10:57:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (22.14%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,40 +1,39 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int last=nums[nums.length-1];
-        int low=0;
-        int high=nums.length-1;
-        while(low<=high){
-            int guess=(low+high)/2;
-            if(nums[guess]==target){
-                return guess;
+      int last=nums[nums.length-1];
+      int low=0;
+      int high=nums.length-1;
+      while(low<=high){
+        int guess=(low+high)/2;
+        if(nums[guess]==target){
+            return guess;
+        }
+        if(nums[guess]>last){
+            if(nums[guess]<target){
+                low=guess+1;
             }
-            if(nums[guess]>last){
-                if(nums[guess]<target){
-                    low=guess+1;
-                }
-                else{
-                    if(nums[0]>target){
-                        low=guess+1;
-                    }
-                    else{
-                        high=guess-1;
-                    }
-                }
+            else{
+                if(nums[0]>target){
+                    low=guess+1;                
             }
-            else {
-                if(nums[guess]>target){
+            else{
+                high=guess-1;
+            }
+        }}
+        else{
+            if(nums[guess]>target){
+                high=guess-1;
+            }
+            else{
+                if(last<target){
                     high=guess-1;
                 }
                 else{
-                 
-                    if(last<target){
-                        high=guess-1;
-                    }
-                    else{
-                        low=guess+1;
-                    }
+                    low=guess+1;
                 }
             }
-        }return -1;
+        }
+      }
+      return -1;
     }
     }
