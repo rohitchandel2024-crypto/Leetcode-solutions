eb2e788431f79b8dd4cbc7f7f587829c982315fe
Rohit Chandel <rohitchandel2024@gmail.com>
2026-02-05 20:09:03 +0530

    Time: 0 ms (100.00%) | Memory: 52.1 MB (18.03%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,37 +1,36 @@
 class Solution {
-
-    int func(int[][] matrix,int n,int m ,int guess){
+    int func(int [] [] matrix,int n,int m,int guess){
         int row=n-1;
         int col=0;
-        int count =0;
+        int count=0;
         while(row>=0 && col<m){
             if(matrix[row][col]<=guess){
                 count+=row+1;
                 col++;
             }
             else{
-                    row--;
+                row--;
             }
-            
-        }return count;
+        }
+        return count;
     }
-
     public int kthSmallest(int[][] matrix, int k) {
-        int n=matrix.length;
-        int m=matrix[0].length;
-        int low=matrix[0][0];
-        int high=matrix[n-1][m-1];
-        int res=-1;
-        while(low<=high){
-            int guess=(low+high)/2;
-            int ans=func(matrix,n,m,guess);
-            if(ans<k){
-                low =guess+1;
-            }
-            else{
-                res=guess;
-                high=guess-1;
-            }
-        }return res;
+      int n=matrix.length;
+      int m=matrix[0].length;
+      int res=-1;
+      int low=matrix[0][0];
+      int high=matrix[n-1][m-1];
+      while(low<=high){
+        int guess=(low+high)/2;
+        int ans=func(matrix,n,m,guess);
+        if(ans<k){
+            low=guess+1;
+        }
+        else{
+            res=guess;
+            high=guess-1;
+        }
+      }
+      return res;
     }
 }
