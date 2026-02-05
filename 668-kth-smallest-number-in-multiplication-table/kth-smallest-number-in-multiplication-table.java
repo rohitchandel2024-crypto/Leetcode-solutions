b8a413609e1d5ecd746056ed7d7eb621535c91d8
Rohit Chandel <rohitchandel2024@gmail.com>
2026-02-05 20:40:27 +0530

    Time: 10 ms (54.56%) | Memory: 40.1 MB (100.00%) - LeetSync

--- 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.java
+++ 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.java
@@ -1,28 +1,27 @@
 class Solution {
-
     int func(int n,int m,int guess){
         int count=0;
         for(int i=1;i<=m;i++){
             count+=Math.min(n,guess/i);
+
         }
         return count;
     }
-
     public int findKthNumber(int m, int n, int k) {
-        int low=1;
-        int high=m*n;
-        int res=-1;
-        while(low<=high){
-            int guess = (low+high)/2;
-            int ans=func(n,m,guess);
-            if(ans<k){
-                low=guess+1;
-            }
-            else{
-                res = guess;
-                high=guess-1;
-            }
+     int low=1;
+     int res=-1;
+     int high=n*m;
+     while(low<=high){
+        int guess=(low+high)/2;
+        int ans=func(n,m,guess);
+        if(ans<k){
+            low=guess+1;
         }
-        return res;
+        else{
+            res=guess;
+            high=guess-1;
+        }
+     }
+     return res;
     }
 }
