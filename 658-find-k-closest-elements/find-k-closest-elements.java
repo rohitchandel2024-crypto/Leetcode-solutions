0a92a700ad556c453d0aeed114af9f30515145ae
Rohit Chandel <rohitchandel2024@gmail.com>
2026-02-09 14:09:08 +0530

    Time: 5 ms (58.68%) | Memory: 47.4 MB (98.14%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -2,7 +2,7 @@ class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         int low = 0, high = arr.length - 1;
 
-        // lower_bound binary search
+     
         while (low <= high) {
             int mid = (low + high) / 2;
             if (arr[mid] < x) low = mid + 1;
@@ -12,19 +12,18 @@ class Solution {
         int left = high;
         int right = low;
 
-        // EXPAND window
         while (right - left - 1 < k) {
             if (left < 0) {
-                right++;                // ✅ expand right
+                right++;              
             } 
             else if (right >= arr.length) {
-                left--;                 // ✅ expand left
+                left--;                 
             } 
             else if (Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)) {
-                left--;                 // ✅ closer on left
+                left--;                
             } 
             else {
-                right++;                // ✅ closer on right
+                right++;               
             }
         }
 
