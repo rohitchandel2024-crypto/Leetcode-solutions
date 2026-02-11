8a7e7b9bdc1d30be149235e0730af14da70ee611
Rohit Chandel <rohitchandel2024@gmail.com>
2026-02-11 10:18:04 +0530

    Time: 87 ms (92.92%) | Memory: 135.8 MB (37.22%) - LeetSync

---
+++ 502-ipo/ipo.java
@@ -0,0 +1,39 @@
+class Pair{
+    int first;
+    int second;
+    Pair(int f,int s){
+        first=f;
+        second=s;
+    }
+}
+    
+    class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        PriorityQueue <Integer> pq= new PriorityQueue<>(
+            (a,b)->b-a
+        );
+        ArrayList<Pair> arr=new ArrayList<>();
+        int n=capital.length;
+        for(int i=0;i<n;i++){
+            arr.add(new Pair(capital[i],profits[i]));
+        }
+       Collections.sort(arr,(a,b)->a.first-b.first);
+        int idx=0;
+        while(k-->0 ){
+            while(idx<n){
+            if(arr.get(idx).first>w) break;
+            pq.add(arr.get(idx).second);
+            idx++;
+            }
+
+            if(pq.isEmpty()){
+                return w;
+            }
+            w=w+pq.peek();
+             pq.poll();
+        }
+           
+        
+        return w;
+    }
+}
