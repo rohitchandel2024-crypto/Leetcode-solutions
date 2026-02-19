ef398e81a31942dd94f385e78ae3dc5009ca7498
Rohit Chandel <rohitchandel2024@gmail.com>
2026-02-19 10:38:44 +0530

    Time: 7 ms (73.54%) | Memory: 130.7 MB (65.48%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -1,30 +1,33 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        Stack<ListNode> st=new Stack<>();
-        while(head!=null){
-            while(!st.isEmpty() && st.peek().val<head.val){
-                st.pop();
-            }
-            st.push(head);
-            head = head.next;
+       head= reverse(head);
 
+        int maxSofar=Integer.MIN_VALUE;
+        ListNode curr = head;
+        ListNode prev=null;
+
+        while(curr!=null){
+            if(curr.val>=maxSofar){
+                maxSofar=curr.val;
+                prev=curr;
+            }
+            else{
+                prev.next=curr.next;
+            }
+            curr=curr.next;
         }
-        ListNode next = null;
-        while(!st.isEmpty()){
-            ListNode node = st.pop();
-            node.next=next;
-            next=node;
+        return reverse(head);
+   
+    }
+
+    private ListNode reverse(ListNode head){
+        ListNode prev=null;
+        while(head!=null){
+            ListNode next =head.next;
+            head.next=prev;
+            prev=head;
+            head=next;
         }
-        return next;
+        return prev;
     }
 }
