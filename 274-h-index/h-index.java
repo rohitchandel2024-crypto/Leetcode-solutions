7ded54b6d25c7b0804d5643f7fa0fedf036ef7db
Rohit Chandel <rohitchandel2024@gmail.com>
2026-03-05 07:41:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (41.45%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,17 +1,20 @@
 class Solution {
     public int hIndex(int[] citations) {
-      int n=citations.length;
-      int [] freq = new int [n+1];
-      for(int i=0;i<n;i++){
+    int n = citations.length;
+    int [] freq = new int [n+1];
+    for(int i=0;i<n;i++){
         if(citations[i]>=n) freq[n]++;
-        else freq[citations[i]]++;
-      }
-      int h = n;
-      int num = freq[n];
-      while(num<h){
+        else{
+            freq[citations[i]]++;
+        }
+    }
+    int h = n;
+    int num = freq[n];
+    while(num<h){
         h--;
         num+=freq[h];
-      }
-      return h;
+
+    }
+    return h;
     }
 }
