9420b6247085d8b342bee534f7af5c11560328bd
Rohit Chandel <rohitchandel2024@gmail.com>
2026-03-05 08:17:08 +0530

    Time: 0 ms (100.00%) | Memory: 50 MB (93.59%) - LeetSync

--- 275-h-index-ii/h-index-ii.java
+++ 275-h-index-ii/h-index-ii.java
@@ -1,10 +1,14 @@
 class Solution {
     public int hIndex(int[] citations) {
-      int n = citations.length;
-      int i =0;
-      while(i<n && n-i>citations[i]){
-        i++;
-      }
-      return n-i;
+     int n = citations.length;
+     int low = 0;
+     int high = n-1;
+     while(low<=high){
+        int guess = low+(high-low)/2;
+        if(citations[guess]==n-guess) return n-guess;
+        else if (citations[guess]<n-guess) low = guess+1;
+        else high =guess-1;
+     }
+     return n-low;
     }
 }
